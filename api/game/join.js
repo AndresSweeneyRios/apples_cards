668378cead35c0d6d79d7766695ed4ecb52e49c7
Andr3wRiv3rs <andrewarivers@gmail.com>
2020-01-20 21:43:11 -0800

    Persistent joins

--- api/game/join.js
+++ api/game/join.js
@@ -12,36 +12,58 @@ module.exports = (conn, { room }, { rooms }) => {
     conn.room = rooms[room.id]
 
     const { nickname, id } = conn.cookies
-    
-    conn.room.players.push({ 
-        nickname, 
-        id, 
-    })
 
-    const cards = conn.room.whiteDeck.splice(0, 10)
+    if (
+        conn.room.players.filter(
+            player => id === player.id
+        ).length > 0
+    ) {
+        conn.room.clients.splice(
+            conn.room.clients.findIndex(
+                ({ cookies }) => cookies.id === id
+            ),
+            1
+        )
 
-    conn.room.hands.push({
-        nickname, 
-        id, 
-        cards,
-    })
+        conn.reply({
+            event: 'deal',
+            data: {
+                room: conn.room.safe(),
+                cards: conn.room.hands.find( hand => hand.id === id ).cards,
+            }
+        })
+    } else {
+        conn.room.players.push({ 
+            nickname, 
+            id, 
+            score: 0,
+        })
 
-    conn.room.broadcast({
-        event: 'join',
-        data: {
-            players: conn.room.players,
-        }
-    })
+        const cards = conn.room.whiteDeck.splice(0, 10)
+
+        conn.room.hands.push({
+            nickname, 
+            id, 
+            cards,
+        })
+
+        conn.room.broadcast({
+            event: 'join',
+            data: {
+                players: conn.room.players,
+            }
+        })
+
+        conn.reply({
+            event: 'deal',
+            data: {
+                room: conn.room.safe(),
+                cards,
+            }
+        })
+
+        conn.room.message(`<b>${nickname}</b> has joined the game.`)
+    }
 
     conn.room.clients.push(conn)
-
-    conn.reply({
-        event: 'deal',
-        data: {
-            room: conn.room.safe(),
-            cards,
-        }
-    })
-
-    conn.room.message(`<b>${nickname}</b> has joined the game.`)
 }
--- pages/room/[id].js
+++ pages/room/[id].js
@@ -7,11 +7,12 @@ export default ({ ws, connected }) => {
 
     const [blackCard, setBlackCard] = useState({
         text: "What's that sound?",
-        pick: 1
+        pick: 2
     })
 
     const [hand, setHand] = useState([])
     const [room, setRoom] = useState({ })
+    const [picks, setPicks] = useState([{}, {}])
 
     useEffect(() => {
         if (connected) {
@@ -31,12 +32,36 @@ export default ({ ws, connected }) => {
         }
     }, [connected])
 
+    const Card = (content, index, { slotProps, ...restProps } = {}) => <div 
+        className={styles.card}
+        dangerouslySetInnerHTML={{__html: content}}
+        index={index}
+        key={index}
+        { ...restProps }
+    ></div>
+
     const renderHand = () => {
         return hand.map(
-            (content, index) => <div 
-                className={styles.card}
-                dangerouslySetInnerHTML={{__html: content}}
-            ></div>
+            (content, index) => <div className={styles.slot} key={index}> { 
+                Card(content, index, {
+                    style: picks.filter(pick => index === pick.index).length ? {
+                        display: 'none',
+                        pointerEvents: 'none',
+                    }: {},
+
+                    onMouseDown () {
+                        Object.assign(
+                            picks.find(pick => !pick.content) || {},
+                            {
+                                content,
+                                index, 
+                            }
+                        )
+
+                        setPicks([...picks])
+                    },
+                })
+            } </div>
         )
     }
 
@@ -48,8 +73,18 @@ export default ({ ws, connected }) => {
             <div className={styles.hand}>{ renderHand() }</div>
             <div className={styles.game}>
                 <h2 dangerouslySetInnerHTML={{__html: blackCard.text}}></h2>
+                <div className={ styles.picks }>
+                    {picks.map( (pick, index) => <div className={styles.slot} key={index}>
+                        { pick.content ? Card(pick.content, pick.index, {
+                            onMouseDown () {
+                                picks[index] = {}
+                                setPicks([...picks])
+                            },
+                        }) : '' }
+                    </div>)}
+                </div>
             </div>
-            <div className={styles.mobileHand}>{ renderHand() }</div>
+            <div className={styles.mobileHand}>{ renderHand() }<div className={styles.end}></div></div>
             <div className={styles.chat}></div>
         </div>
     </>
